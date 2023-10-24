3a14ed1730c6423121208a41fad7c2778295e082
Shruti-jha6 <122458889+Shruti-jha6@users.noreply.github.com>
2023-10-24 11:50:21 +0530

    Create Selection_sort.java

---
+++ Selection_sort.java
@@ -0,0 +1,39 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the index of the minimum element in the unsorted part of the array
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        System.out.println("Original Array: ");
+        printArray(arr);
+
+        selectionSort(arr);
+
+        System.out.println("Sorted Array: ");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
